01aa8ff5b7014031c620629ccf5614a9935ab8d7
Vanessa de Freitas Ferreira <vanessafreitasferreir4@gmail.com>
2025-08-12 12:20:22 -0400

    ex14

---
+++ ex14.java
@@ -0,0 +1,31 @@
+
+/**
+ * Autor: Vanessa de Freitas Ferreira
+ * Data: 12/08/2025
+ * Projeto: SeuProjetoAqui
+ * Descrição:Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+ * incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+ * impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+ */
+import java.util.Scanner;
+
+public class ex14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("informe uma senha: ");
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha inválida");
+            System.out.println("Digite novamente: ");
+            senha = sc.nextInt();
+
+        }
+        System.out.println("Acesso permitido");
+
+
+
+        sc.close();
+    }
+
+}
